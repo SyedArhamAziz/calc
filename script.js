9d1f0d0fbf878a15f62a9c8753c2912872fa6831
Bob Bytes <bytesbob@gmail.com>
2023-08-20 19:59:43 -0400

    added other functions

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const ops = "+−×÷"
 let elems = document.getElementsByClassName("calc-item");
 let screen = document.getElementById("screen");
 let data = ["", "", ""];
@@ -14,16 +15,25 @@ function onClick() {
             data[2] = data[2] + this.innerHTML;
         }
     }
-    else if (this.className.includes("op")) {
+    else if (this.className.includes("op") && ops.includes(this.innerHTML)) {
         if (data[1] == "") {
             data[1] = this.innerHTML;
         }
         else {
             data[0] = calculate(data);
-            data[1] = "";
+            data[1] = this.innerHTML;
             data[2] = "";
         }
     }
+    else if (this.innerHTML == "C") {
+        resetArr(data);
+    }
+    else if (this.innerHTML == "=") {
+        data[0] = calculate(data);
+        data[1] = "";
+        data[2] = "";
+    }
+    updateDisplay(screen, data);
     console.log(data);
 }
 
@@ -31,5 +41,24 @@ function calculate(arr) {
     switch (arr[1]) {
         case "+":
             return String(Number(arr[0]) + Number(arr[2]));
+        case "−":
+            return String(Number(arr[0]) - Number(arr[2]));
+        case "×":
+            return String(Number(arr[0]) * Number(arr[2]));
+        case "÷":
+            return String(Number(arr[0]) / Number(arr[2]));
+    }
+}
+function updateDisplay(display, arr) {
+    if (arr[2] != "") {
+        display.innerHTML = arr[2];
+    }
+    else {
+        display.innerHTML = arr[0];
+    }
+}
+function resetArr(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = "";
     }
 }
